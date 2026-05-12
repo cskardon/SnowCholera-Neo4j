2252e9e42dca498b859086fe4161357a49a5eea4
Charlotte Skardon <cskardon@xclave.co.uk>
2026-05-12 15:27:43 +0100

    Initial load / transform etc

---
+++ functions.r
@@ -0,0 +1,37 @@
+rotate_coords <- function(e, n, angle_deg) {
+  theta <- angle_deg * pi / 180  # convert to radians
+  e_rot <- e * cos(theta) - n * sin(theta)
+  n_rot <- e * sin(theta) + n * cos(theta)
+  return(data.frame(easting = e_rot, northing = n_rot))
+}
+
+
+generate_latlong <- function(df){
+  #' Generates latitude and longitude columns from the Easting and Northing columns in the SnowCholera data frames.
+  #' 
+  #' @description Uses the Easting and Northing and converts them to Lat/Long.
+  #' 
+  #' NB this does not modify the parameter, it returns an updated version of the data frame.
+  #' 
+  #' @section What it does
+  #' 
+  #' Stuff!
+  #' 
+  #' @param df the data frame to be modified, either the snow_data or pump_data data frames.
+
+  working_df  <- df |> 
+    mutate(Easting = as.integer(Easting), Northing = as.integer(Northing)) |>
+    mutate(rotated = rotate_coords(Easting, Northing, 0)) |> 
+    mutate(sf_points = st_as_sf(rotated, coords = c("easting", "northing"), crs = 27700)) |> 
+    mutate(sf_points_wgs84 = st_transform(sf_points, crs = 4326)) |> 
+    mutate(
+        longitude = st_coordinates(sf_points_wgs84)[,1], 
+        latitude = st_coordinates(sf_points_wgs84)[,2]
+    ) |> 
+    mutate(
+        sf_points = NULL,
+        sf_points_wgs84 = NULL,
+        rotated = NULL
+    )
+  return(working_df)
+}
